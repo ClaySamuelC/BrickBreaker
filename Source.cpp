ff632e188fa5313afcab0eb31eca7d222854e92c
ClaySamuelC <44789280+ClaySamuelC@users.noreply.github.com>
2021-07-11 13:50:44 -0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,68 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+class BrickBreaker : public olc::PixelGameEngine
+{
+public:
+	BrickBreaker()
+	{
+		sAppName = "BrickBreaker";
+	}
+
+	olc::vi2d fieldSize = { 400, 600 };
+	
+	olc::vd2d playerPos = { fieldSize.x / 2, fieldSize.y * 0.9 };
+	olc::vd2d ballPos = { fieldSize.x / 2, fieldSize.y / 2 };
+	olc::vd2d ballVel = { 0, 100.0f };
+
+	olc::vd2d playerSize = { 80.0, 32.0 };
+
+	olc::vi2d playerRadius = { playerSize.x / 2, playerSize.y / 2 };
+	
+	float playerVel = 200.0f;
+
+public:
+	bool OnUserCreate() override
+	{
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// Input
+		if (GetKey(olc::LEFT).bHeld)
+		{
+			playerPos.x -= (playerVel * double(fElapsedTime));
+		}
+		if (GetKey(olc::RIGHT).bHeld)
+		{
+			playerPos.x += (playerVel * double(fElapsedTime));
+		}
+
+		// Game Logic
+		ballPos += ballVel * fElapsedTime;
+
+		// Collisions
+		if (playerPos.x - playerRadius.x < 0)
+			playerPos.x = playerRadius.x;
+		if (playerPos.x + playerRadius.x >= fieldSize.x)
+			playerPos.x = fieldSize.x - playerRadius.x - 1;
+
+		// Draw
+		Clear(olc::GREY);
+		FillRect(playerPos - playerRadius, playerSize, olc::YELLOW);
+		FillCircle(ballPos, 10, olc::BLACK);
+
+		return true;
+	}
+};
+
+
+int main()
+{
+	BrickBreaker demo;
+	if (demo.Construct(demo.fieldSize.x, demo.fieldSize.y, 1, 1))
+		demo.Start();
+
+	return 0;
+}
